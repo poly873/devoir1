46172ce6be8eb7fac64e8ba5e72875688551b513
poly873 <guillaume.carrier-couture@etud.polymtl.ca>
2025-10-12 21:58:30 -0400

    Déploiement de la VM

---
+++ vm.tf
@@ -0,0 +1,40 @@
+resource "azurerm_network_interface" "nic_pipeline" {
+  name                = "cr460_vm_nic"
+  location            = azurerm_resource_group.rg_pipeline.location
+  resource_group_name = azurerm_resource_group.rg_pipeline.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_pipeline.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm_pipeline" {
+  name                = "cr460_vm"
+  computer_name       = "cr460-vm"
+  location            = azurerm_resource_group.rg_pipeline.location
+  resource_group_name = azurerm_resource_group.rg_pipeline.name
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic_pipeline.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("${path.module}/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
