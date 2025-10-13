cb364a18c62d58415d526997f35d9182a9306a46
poly873 <guillaume.carrier-couture@etud.polymtl.ca>
2025-10-12 21:03:25 -0400

    Déploiement du Resource Group

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "cr460_devoir1 2025"
+    workspaces {
+      name = "devoir1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg_pipeline" {
+  name     = "cr460_resourcegroup"
+  location = "Canada East"
+}
